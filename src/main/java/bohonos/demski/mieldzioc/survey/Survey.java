aad84efbbb7c4a56b6a8eb5e919807ed27587b71
andrzej1990 <andrzej.bohonos1990@gmail.com>
2015-04-11 17:05:40 +0200

    Survey class added

--- src/main/java/bohonos/demski/mieldzioc/survey/Survey.java
+++ src/main/java/bohonos/demski/mieldzioc/survey/Survey.java
@@ -5,12 +5,34 @@ package bohonos.demski.mieldzioc.survey;
 
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Objects;
 
+import bohonos.demski.mieldzioc.questions.Question;
+
 /**
- * @author 
+ * @author Andrzej Bohonos
  *
  */
 public class Survey {
+    
+    public List<Question> questions = new ArrayList<Question>();
+    private GregorianCalendar startTime;
+    private GregorianCalendar finishTime;
+    //public Interviewer interviewer;
+    private String title;
+    private String description;
+    private String summary;
+    private int numberOfSurvey;
+    
+    public boolean startSurvey()
+    {
+        return true;
+    }
+    
+    public boolean finishSurvey()
+    {
+        return true;
+    }
 
 }
