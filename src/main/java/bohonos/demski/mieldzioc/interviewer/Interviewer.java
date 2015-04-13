1b5c35c3db9624470129350beeb2b5b8194e9206
Delirus13 <adamm90110@gmail.com>
2015-04-13 20:27:10 +0200

    Dodanie getterów i setterów w pakiecie Interview

--- src/main/java/bohonos/demski/mieldzioc/interviewer/Interviewer.java
+++ src/main/java/bohonos/demski/mieldzioc/interviewer/Interviewer.java
@@ -18,35 +18,37 @@ import java.util.Map.Entry;
  * @author Delirus
  */
 public class Interviewer {
-    public String name, surname;
-    public int id; //PESEL
-    public GregorianCalendar hiredDay;
-    public GregorianCalendar relieveDay=null;
+    private String name, surname;
+    private int id; //PESEL
+    private GregorianCalendar hiredDay;
+    private GregorianCalendar relieveDay=null;
     //private boolean isActive;
-    public List<Entry<GregorianCalendar,GregorianCalendar>> outOfWorkTime = new ArrayList<Entry<GregorianCalendar,GregorianCalendar>>();
+    private List<Entry<GregorianCalendar,GregorianCalendar>> outOfWorkTime = new ArrayList<Entry<GregorianCalendar,GregorianCalendar>>();
     //private List<Pair<GregorianCalendar,GregorianCalendar>> outOfWorkTime = new ArrayList<>();
     //public InterviewerSurveyPrivileges privileges = new InterviewerSurveyPrivileges();
-    public Map<Integer, InterviewerSurveyPrivileges> intervSurveyPrivileges = new TreeMap<Integer, InterviewerSurveyPrivileges>(); //ewentuanie HashMap
-    public InterviewerPrivileges privileges = new InterviewerPrivileges();
+    private Map<Integer, InterviewerSurveyPrivileges> intervSurveyPrivileges = new TreeMap<Integer, InterviewerSurveyPrivileges>(); //ewentuanie HashMap
+    private InterviewerPrivileges privileges = new InterviewerPrivileges();
+   
+   /**
+    * Konstruktor klasy Interviewer
+    * @param name
+    * @param surname
+    * @param id
+    * @param hireday 
+    */ 
     Interviewer(String name, String surname, int id, GregorianCalendar hireday)
     {
         this.name=name;
         this.surname=surname;
         this.id=id;
         this.hiredDay=hireday;
-        //isActive=true;
     }
 
-    Interviewer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    public void setOutOfWorkTime (GregorianCalendar g1, GregorianCalendar g2){
-        outOfWorkTime.add(new SimpleEntry<GregorianCalendar, GregorianCalendar>(g1, g2));
-    }
-    
-    public void setPrivilegesForInterviewer(int numberOfSurvey, InterviewerSurveyPrivileges privileges){
-        intervSurveyPrivileges.put(id, privileges);
-    }
+
+    /**
+     * Metoda isActive() na podstawie okresów czasu z outOfWorkTime zwraca wartoœæ true jeœli ankieter jest aktywny, w p.p. zwraca false. 
+     * @return 
+     */
     public boolean isActive(){
         GregorianCalendar now = new GregorianCalendar();
         for (Entry<GregorianCalendar, GregorianCalendar> entr : outOfWorkTime)
@@ -57,4 +59,58 @@ public class Interviewer {
         return true;
     }
     
+     public String getName(){
+        return name;
+    }
+     
+     public String getSurname(){
+        return surname;
+    }
+   
+    public int getId(){
+        return id;
+    }
+    
+    public GregorianCalendar getHiredDay(){
+        return hiredDay;
+    }
+    
+    public GregorianCalendar getRelieveDay(){
+        return relieveDay;
+    }
+     
+    public List<Entry<GregorianCalendar,GregorianCalendar>> getOutOfWorkTime(){
+        return outOfWorkTime;
+    }
+      
+    public Map<Integer, InterviewerSurveyPrivileges> getIntervSurveyPrivileges(){
+        return intervSurveyPrivileges;
+    }
+    
+    public void editeName(String name){
+        this.name=name;
+    }
+    
+    public void editeSurname(String surname){
+        this.surname=surname;
+    }
+    
+    public void editeId(int id){
+        this.id=id;
+    }
+    
+    public void editeHireDay(GregorianCalendar hiredDay){
+        this.hiredDay=hiredDay;
+    }
+    public void setRelieveDay(GregorianCalendar relieveDay){
+        this.relieveDay=relieveDay;
+    }
+     public void setOutOfWorkTime (GregorianCalendar g1, GregorianCalendar g2){
+        outOfWorkTime.add(new SimpleEntry<GregorianCalendar, GregorianCalendar>(g1, g2));
+    }
+    
+    public void setPrivilegesForInterviewer(int numberOfSurvey, InterviewerSurveyPrivileges privileges){
+        intervSurveyPrivileges.put(id, privileges);
+    }
+    //brakuje edytowania outOfWorkTime i intervSurveyPrivileges
 }
--- src/main/java/bohonos/demski/mieldzioc/interviewer/InterviewersRepository.java
+++ src/main/java/bohonos/demski/mieldzioc/interviewer/InterviewersRepository.java
@@ -20,36 +20,89 @@ import java.util.List;
 public class InterviewersRepository {
     private List<Interviewer> interviewers = new ArrayList<Interviewer>();
     
-    public void addInterviewer (Interviewer interviewer){
-        interviewers.add(interviewer);
+    /**
+     * Metoda addInterviewer() dodaje ankietera do listy. Jeœli siê uda³o dodaæ ankietera do listy (nie ma na liœcie ankieteterów kogoœ o tym samym id), to metoda zwraca true, w przeciwnym wypadku false.
+     * @param interviewer
+     * @return 
+     */
+    public boolean addInterviewer (Interviewer interviewer){
+        if(checkListInterviewers(interviewer))
+            return false;
+        else{
+            interviewers.add(interviewer);
+            return true;
+        }
+            
     }
-    
+    /**
+     * Dostajemy ankietera o podanym id.
+     * @param idi
+     * @return 
+     */
     public Interviewer getInterviewer(int idi){
         for(Interviewer person : interviewers)
         {
-            if(person.id==idi)
+            if(person.getId()==idi)
                 return person;
         }
         return null;
     }
+    
   public List<Interviewer> getAllInterviewers(){
       return interviewers;
   }
+  /**
+   * U¿ywaj¹c metody getAllInterviewers() z parametrem from otrzymujemy listê wszystkich zatrudnionych aniekterów od podanego dnia
+   * @param from
+   * @return 
+   */
   public List<Interviewer> getAllInterviewers(GregorianCalendar from){ 
       List<Interviewer> lista = new ArrayList<Interviewer>();
       for(Interviewer person : interviewers){
-          if(from.compareTo(person.hiredDay)<=0)
-              lista.add(person);
-      }
-      return lista;
-  }
-  public List<Interviewer> getAllInterviewers(GregorianCalendar from, GregorianCalendar to){
-      List<Interviewer> lista = new ArrayList<Interviewer>();
-      for(Interviewer person : interviewers){
-          if(from.compareTo(person.hiredDay)>=0 && to.compareTo(person.hiredDay)<=0) //sprawdŸ ten warunek
+          if(from.compareTo(person.getHiredDay())<=0)
               lista.add(person);
       }
       return lista;
   }
   
+  /**
+   * U¿ywaj¹c metody getAllInterviewers() z parametrami from i to, dostajemy listê ankieterów zatrudnionych od dnia from do dnia to w³¹cznie z tym dniem. 
+   * @param from
+   * @param to
+   * @return 
+   */
+  public List<Interviewer> getAllInterviewers(GregorianCalendar from, GregorianCalendar to){
+      List<Interviewer> lista = new ArrayList<Interviewer>();
+      for(Interviewer person : interviewers){
+          if(from.compareTo(person.getHiredDay())>=0 && to.compareTo(person.getHiredDay())<=0) //sprawdŸ ten warunek
+              lista.add(person);
+      }
+      return lista;
+  }
+  /**
+   * Metoda checkListInterviewers() sprawdza czy podany ankieter nie znajduje siê ju¿ przypadkiem na liœcie. Jeœli siê znajduje, to dostajemy true. W przeciwnym wypadku false. 
+   * @param man
+   * @return 
+   */
+  public boolean checkListInterviewers(Interviewer man){
+      for(Interviewer person : interviewers){
+          if(man.getId()==person.getId())
+              return true;
+      }
+      return false;
+  }
+  
+  /**
+   * Metoda equalsInterviewers porównuje dwóch ankieterów po id i zwraca true, jeœli s¹ to Ci sami ankieterzy, afalse gdy nie s¹.
+   * @param man1
+   * @param man2
+   * @return 
+   */
+  public boolean equalsInterviewers(Interviewer man1,Interviewer man2){
+      if(man1.getId()==man2.getId())
+          return true;
+      else
+          return false;
+  }
+  //edytowanie listy , usuwanie ankieterów i te sprawy trzeba dodaæ
 }
