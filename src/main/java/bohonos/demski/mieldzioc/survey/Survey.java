2fa32ce92494c84809191059486a9d7402a9b17b
Dominik Demski <dominik.demski@interia.pl>
2015-04-14 12:35:11 +0200

    dodanie kontruktora do klasy Survey

--- src/main/java/bohonos/demski/mieldzioc/survey/Survey.java
+++ src/main/java/bohonos/demski/mieldzioc/survey/Survey.java
@@ -17,7 +17,8 @@ import bohonos.demski.mieldzioc.interviewer.Interviewer;
  */
 public class Survey {
     
-    public List<Question> questions = new ArrayList<Question>();
+	
+	public List<Question> questions = new ArrayList<Question>();
     private GregorianCalendar startTime = null;
     private GregorianCalendar finishTime = null;
     private Interviewer interviewer;
@@ -144,7 +145,7 @@ public class Survey {
     {
         this.interviewer = interviewer;
     }
-    
+ 
     public Survey(List<Question> questions, GregorianCalendar startTime, GregorianCalendar finishTime, Interviewer interviewer, int idOfSurveys, String title, String description, String summary, int numberOfSurvey)
     {
         this.questions = questions;
@@ -157,5 +158,16 @@ public class Survey {
         this.description = description;
         this.numberOfSurvey = numberOfSurvey;
     }
+    
+    /**
+	 * Stwórz now¹ ankietê, podaj¹c jako argument ankietera tworz¹cego ankietê. 
+	 * Jeœli tworzony jest szablon ankiety, stworzon¹ ankietê nale¿y przekazaæ klasie SurveyHandler,
+	 * ona nada szablonowi numer grupy ankiet. 
+	 * @author Dominik Demski
+	 * @param interviewer - Ankieter, który stworzy³ dan¹ ankietê.
+	 */
+    public Survey(Interviewer interviewer) {
+		this.interviewer = interviewer;
+	}
 
 }
