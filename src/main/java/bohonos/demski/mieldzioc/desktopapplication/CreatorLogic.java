1ee291660151455f25d0b283eca7ee90a3419bba
andrzej1990 <andrzej.bohonos1990@gmail.com>
2015-05-07 07:17:43 +0200

    Creator - part 6

--- src/main/java/bohonos/demski/mieldzioc/desktopapplication/CreatorLogic.java
+++ src/main/java/bohonos/demski/mieldzioc/desktopapplication/CreatorLogic.java
@@ -5,10 +5,33 @@
  */
 package bohonos.demski.mieldzioc.desktopapplication;
 
+import bohonos.demski.mieldzioc.survey.SurveyHandler;
+import bohonos.demski.mieldzioc.survey.Survey;
+import bohonos.demski.mieldzioc.interviewer.Interviewer;
+import bohonos.demski.mieldzioc.questions.Question;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Andrzej
  */
 public class CreatorLogic {
+    
+    private SurveyHandler surveyHandler;
+    private Interviewer interviewer;
+    
+    /**
+     * adds new survey template to survey handler
+     * @return id of new survey template
+     */
+    public String newSurvey() {
+        String idOfSurvey = surveyHandler.addNewSurveyTemplate(new Survey(interviewer));
+        return idOfSurvey;
+    }
+    
+    public CreatorLogic() {
+        surveyHandler = new SurveyHandler(0);
+        interviewer = new Interviewer("Imiê", "Nazwisko", "PESEL000000", new GregorianCalendar()); //to do
+    }
     
 }
