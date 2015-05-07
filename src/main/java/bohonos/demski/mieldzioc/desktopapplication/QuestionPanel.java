8dfccf8524969077c2234cfb4351d7229779e00e
andrzej1990 <andrzej.bohonos1990@gmail.com>
2015-05-07 04:34:05 +0200

    Creator - part 2

---
+++ src/main/java/bohonos/demski/mieldzioc/desktopapplication/QuestionPanel.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bohonos.demski.mieldzioc.desktopapplication;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Andrzej
+ */
+public class QuestionPanel extends JPanel implements ActionListener {
+    
+    CreatorLogic creatorLogic;
+    
+    private JButton questionUp;
+    private JButton questionDown;
+    private JButton questionDelete;
+    
+    public QuestionPanel(CreatorLogic cl) {
+        
+        creatorLogic = cl;
+        
+    }
+
+    /**
+     * adds questionUp button to ActionListener from other panel and returns this button
+     * @param al given ActionListener
+     * @return reference to questionUp button
+     */
+    public JButton addActionListenerQuestionUp(ActionListener al) {
+        questionUp.addActionListener(al);
+        return questionUp;
+    }
+    
+    /**
+     * adds questionDown button to ActionListener from other panel and returns this button
+     * @param al given ActionListener
+     * @return reference to questionDown button
+     */
+    public JButton addActionListenerQuestionDown(ActionListener al) {
+        questionDown.addActionListener(al);
+        return questionDown;
+    }
+    
+    /**
+     * adds questionDelete button to ActionListener from other panel and returns this button
+     * @param al given ActionListener
+     * @return reference to questionDelete button
+     */
+    public JButton addActionListenerQuestionDelete(ActionListener al) {
+        questionDelete.addActionListener(al);
+        return questionDelete;
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        
+    }
+    
+}
---
+++ src/main/java/bohonos/demski/mieldzioc/desktopapplication/SurveyPanel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bohonos.demski.mieldzioc.desktopapplication;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Andrzej
+ */
+public class SurveyPanel extends JPanel implements ActionListener {
+    
+    CreatorLogic creatorLogic;
+    
+    public SurveyPanel(CreatorLogic cl) {
+        
+        creatorLogic = cl;
+        
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+    
+}
