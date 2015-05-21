90947dab22d9921e489cfc38d2574f342561b8b4
andrzej1990 <andrzej.bohonos1990@gmail.com>
2015-05-21 08:15:02 +0200

    question panels added

---
+++ src/main/java/bohonos/demski/mieldzioc/desktopapplication/MultipleChoiceQuestionPanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bohonos.demski.mieldzioc.desktopapplication;
+
+/**
+ *
+ * @author Andrzej
+ */
+public class MultipleChoiceQuestionPanel extends QuestionPanel {
+
+    public MultipleChoiceQuestionPanel(ApplicationLogic appLogic) {
+        super(appLogic);
+    }
+    
+}
---
+++ src/main/java/bohonos/demski/mieldzioc/desktopapplication/ScaleQuestionPanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bohonos.demski.mieldzioc.desktopapplication;
+
+/**
+ *
+ * @author Andrzej
+ */
+public class ScaleQuestionPanel extends QuestionPanel {
+
+    public ScaleQuestionPanel(ApplicationLogic appLogic) {
+        super(appLogic);
+    }
+    
+}
