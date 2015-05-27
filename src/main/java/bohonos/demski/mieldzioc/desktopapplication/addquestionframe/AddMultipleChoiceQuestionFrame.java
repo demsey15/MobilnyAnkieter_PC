daa6395833373e303d2db2d4cb739cfdb72d0c80
andrzej1990 <andrzej.bohonos1990@gmail.com>
2015-05-27 23:02:14 +0200

    AddAnswerFrame - part 1

--- src/main/java/bohonos/demski/mieldzioc/desktopapplication/addquestionframe/AddMultipleChoiceQuestionFrame.java
+++ src/main/java/bohonos/demski/mieldzioc/desktopapplication/addquestionframe/AddMultipleChoiceQuestionFrame.java
@@ -9,7 +9,9 @@ import bohonos.demski.mieldzioc.desktopapplication.ApplicationLogic;
 import bohonos.demski.mieldzioc.desktopapplication.CreatorFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
@@ -17,12 +19,33 @@ import javax.swing.JFrame;
  */
 public class AddMultipleChoiceQuestionFrame extends AddQuestionFrame {
     
+    private List<String> answers;
+    
+    private JLabel answerLabel;
     
     public AddMultipleChoiceQuestionFrame(ApplicationLogic appLogic, CreatorFrame crFrame) {
         
         super(appLogic, crFrame);
         
+        this.setSize(600, 500);
         
+        answerLabel = new JLabel("mo¿liwe odpowiedzi: ");
+        answerLabel.setBounds(LABELS_X_POSITION, CURRENT_Y_POSITION, LABELS_WIDTH, LABELS_HEIGHT);
+        this.add(answerLabel);
+        
+        // to do
+        
+        CURRENT_Y_POSITION = CURRENT_Y_POSITION + FIELDS_HEIGHT + SPACE_HEIGHT;
+
+        addButton.setBounds(ADD_BUTTON_X_POSITION, CURRENT_Y_POSITION + SPACE_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT);
+
+        cancelButton.setBounds(CANCEL_BUTTON_X_POSITION, CURRENT_Y_POSITION + SPACE_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT);
+        
+    }
+    
+    @Override
+    protected Boolean questionConditions() {
+        return true;
     }
 
 }
