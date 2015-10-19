66e9bd4ddf2b7cece52b29f731d126787467102d
demsey15 <dominik.demski@interia.pl>
2015-10-19 10:22:44 +0200

    Pair class refactored

--- src/main/java/bohonos/demski/mieldzioc/common/Pair.java
+++ src/main/java/bohonos/demski/mieldzioc/common/Pair.java
@@ -1,37 +1,28 @@
-/**
- * 
- */
 package bohonos.demski.mieldzioc.common;
 
 import java.util.Objects;
 
 /**
  * @author Dominik Demski
- * 
  */
 public class Pair<T, U> {
-	
 	private T first;
 	private U second;
 
-	/**
-	 * 
-	 */
 	public Pair() {
 		first = null;
 		second = null;
 	}
-	
 
-	public Pair(T first){
+	public Pair(T first) {
 		this(first, null);
 	}
-	
-	public Pair(T first, U second){
+
+	public Pair(T first, U second) {
 		this.first = first;
 		this.second = second;
 	}
-	
+
 	public T getFirst() {
 		return first;
 	}
@@ -48,16 +39,21 @@ public class Pair<T, U> {
 		this.second = second;
 	}
 
-	
 	@Override
 	public boolean equals(Object o) {
-		if(this == o) return true;
-		if(o == null) return false;
-		if(this.getClass() != o.getClass()) return false;
-		
+		if (this == o){
+			return true;
+		}
+		if (o == null){
+			return false;
+		}
+		if (this.getClass() != o.getClass()){
+			return false;
+		}
+
 		@SuppressWarnings("rawtypes")
 		Pair o2 = (Pair) o;
-		
+
 		return Objects.equals(first, o2.first) && Objects.equals(second, o2.second);
 	}
 }
